39420d9c8e43e9521202859b13ad176cbe570c51
taner <taner@TANER>
2021-11-13 10:25:48 +0300

    Add project files.

---
+++ MobilCode/frmMain.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace MobilCode
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSil = new DevExpress.XtraEditors.SimpleButton();
+            this.btnGuncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnList = new DevExpress.XtraEditors.SimpleButton();
+            this.txtId = new DevExpress.XtraEditors.TextEdit();
+            this.txtAdi = new DevExpress.XtraEditors.TextEdit();
+            this.txtSoyadi = new DevExpress.XtraEditors.TextEdit();
+            this.txtMeil = new DevExpress.XtraEditors.TextEdit();
+            this.txtTelefon = new DevExpress.XtraEditors.TextEdit();
+            this.txtGorsel = new DevExpress.XtraEditors.TextEdit();
+            this.txtDepartman = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.txtToplamPersonel = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtId.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAdi.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSoyadi.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMeil.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTelefon.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGorsel.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDepartman.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtToplamPersonel.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(741, 350);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnEkle.ImageOptions.Image")));
+            this.btnEkle.Location = new System.Drawing.Point(656, 530);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(97, 23);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnSil.ImageOptions.Image")));
+            this.btnSil.Location = new System.Drawing.Point(656, 490);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(97, 23);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnGuncelle.ImageOptions.Image")));
+            this.btnGuncelle.Location = new System.Drawing.Point(656, 446);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(97, 23);
+            this.btnGuncelle.TabIndex = 3;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnList.ImageOptions.Image")));
+            this.btnList.Location = new System.Drawing.Point(656, 406);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(97, 23);
+            this.btnList.TabIndex = 4;
+            this.btnList.Text = "Listele";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(114, 412);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(114, 449);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtAdi.TabIndex = 6;
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(114, 493);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyadi.TabIndex = 7;
+            // 
+            // txtMeil
+            // 
+            this.txtMeil.Location = new System.Drawing.Point(114, 532);
+            this.txtMeil.Name = "txtMeil";
+            this.txtMeil.Size = new System.Drawing.Size(100, 20);
+            this.txtMeil.TabIndex = 8;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(114, 583);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefon.TabIndex = 9;
+            // 
+            // txtGorsel
+            // 
+            this.txtGorsel.Location = new System.Drawing.Point(427, 412);
+            this.txtGorsel.Name = "txtGorsel";
+            this.txtGorsel.Size = new System.Drawing.Size(100, 20);
+            this.txtGorsel.TabIndex = 10;
+            // 
+            // txtDepartman
+            // 
+            this.txtDepartman.Location = new System.Drawing.Point(427, 438);
+            this.txtDepartman.Name = "txtDepartman";
+            this.txtDepartman.Size = new System.Drawing.Size(100, 20);
+            this.txtDepartman.TabIndex = 11;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(37, 415);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(18, 13);
+            this.labelControl1.TabIndex = 12;
+            this.labelControl1.Text = "ID :";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(37, 452);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(22, 13);
+            this.labelControl2.TabIndex = 13;
+            this.labelControl2.Text = "Adi :";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(37, 498);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(39, 13);
+            this.labelControl3.TabIndex = 14;
+            this.labelControl3.Text = "Soyadı :";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(37, 537);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(25, 13);
+            this.labelControl4.TabIndex = 15;
+            this.labelControl4.Text = "Meil :";
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(37, 594);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(43, 13);
+            this.labelControl5.TabIndex = 16;
+            this.labelControl5.Text = "Telefon :";
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Location = new System.Drawing.Point(334, 411);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(37, 13);
+            this.labelControl6.TabIndex = 17;
+            this.labelControl6.Text = "Görsel :";
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Location = new System.Drawing.Point(334, 441);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(60, 13);
+            this.labelControl7.TabIndex = 18;
+            this.labelControl7.Text = "Departman :";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(334, 490);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(96, 23);
+            this.simpleButton1.TabIndex = 19;
+            this.simpleButton1.Text = "Toplam Personel :";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // txtToplamPersonel
+            // 
+            this.txtToplamPersonel.Location = new System.Drawing.Point(436, 487);
+            this.txtToplamPersonel.Name = "txtToplamPersonel";
+            this.txtToplamPersonel.Size = new System.Drawing.Size(100, 20);
+            this.txtToplamPersonel.TabIndex = 20;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 620);
+            this.Controls.Add(this.txtToplamPersonel);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.labelControl7);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txtDepartman);
+            this.Controls.Add(this.txtGorsel);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtMeil);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtId.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAdi.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSoyadi.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMeil.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTelefon.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGorsel.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDepartman.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtToplamPersonel.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DevExpress.XtraEditors.SimpleButton btnEkle;
+        private DevExpress.XtraEditors.SimpleButton btnSil;
+        private DevExpress.XtraEditors.SimpleButton btnGuncelle;
+        private DevExpress.XtraEditors.SimpleButton btnList;
+        private DevExpress.XtraEditors.TextEdit txtId;
+        private DevExpress.XtraEditors.TextEdit txtAdi;
+        private DevExpress.XtraEditors.TextEdit txtSoyadi;
+        private DevExpress.XtraEditors.TextEdit txtMeil;
+        private DevExpress.XtraEditors.TextEdit txtTelefon;
+        private DevExpress.XtraEditors.TextEdit txtGorsel;
+        private DevExpress.XtraEditors.TextEdit txtDepartman;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.TextEdit txtToplamPersonel;
+    }
+}
---
+++ MobilCode/frmMain.cs
@@ -0,0 +1,138 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MobilCode
+{
+    public partial class frmMain : DevExpress.XtraEditors.XtraForm
+    {
+        SqlConnection con = new SqlConnection("data source=.;initial catalog=DbisTakip;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+        SqlDataAdapter adtr;
+        SqlCommand cmd;
+        DataTable tablo = new DataTable();
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            ListelePersonel();
+
+            int x = 0;
+            x = dataGridView1.Rows.Count;
+
+            txtToplamPersonel.Text = x.ToString();
+
+        }
+
+        public void EklePersonel()
+        {
+            con.Open();
+            cmd = new SqlCommand("EklePersonel", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ID",txtId);
+            cmd.Parameters.AddWithValue("@Adi", txtAdi);
+            cmd.Parameters.AddWithValue("@Soyadi", txtSoyadi);
+            cmd.Parameters.AddWithValue("@Meil", txtMeil);
+            cmd.Parameters.AddWithValue("@Telefon", txtTelefon);
+            cmd.Parameters.AddWithValue("@Gorsel", txtGorsel); 
+            cmd.Parameters.AddWithValue("@Departman", txtDepartman);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Başarı ile eklendi");
+            ListelePersonel();
+
+        }
+        public void Guncelle()
+        {
+
+        }
+        public void SilPersonel()
+        {
+            con.Open();
+            cmd = new SqlCommand("SilPersonel", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ID",dataGridView1.CurrentRow.Cells[0].Value);
+          
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Başarı ile silindi.");
+            ListelePersonel();
+
+        }
+        public void ListelePersonel()
+        {
+            tablo.Clear();
+            adtr = new SqlDataAdapter("GosterPersonel", con);
+            adtr.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand("EklePersonel", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.AddWithValue("@ID", txtId.Text);
+            cmd.Parameters.AddWithValue("@Adi", txtAdi.Text);
+            cmd.Parameters.AddWithValue("@Soyadi", txtSoyadi.Text);
+            cmd.Parameters.AddWithValue("@Meil", txtMeil.Text);
+            cmd.Parameters.AddWithValue("@Telefon", txtTelefon.Text);
+            cmd.Parameters.AddWithValue("@Gorsel", txtGorsel.Text);
+            cmd.Parameters.AddWithValue("@Departman", txtDepartman.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Başarı ile eklendi");
+            ListelePersonel();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            SilPersonel();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand("GuncellePersonel", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ID", txtId.Text);
+            cmd.Parameters.AddWithValue("@Adi", txtAdi.Text);
+            cmd.Parameters.AddWithValue("@Soyadi", txtSoyadi.Text);
+            cmd.Parameters.AddWithValue("@Meil", txtMeil.Text);
+            cmd.Parameters.AddWithValue("@Telefon", txtTelefon.Text);
+            cmd.Parameters.AddWithValue("@Gorsel", txtGorsel.Text);
+            cmd.Parameters.AddWithValue("@Departman", txtDepartman.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Başarı ile GUncellendi");
+            ListelePersonel();
+        }
+
+        private void textEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+
+            con.Open();
+            cmd = new SqlCommand("Select count(*) from TblPersonel", con);
+            Int32 count = (Int32)cmd.ExecuteScalar();
+            txtToplamPersonel.Text = Convert.ToString(count );
+            con.Close();
+           
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
